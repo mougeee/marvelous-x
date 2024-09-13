ad57354662690b94bc417979c45c086b0a27a243
Sch <junhg0211@gmail.com>
2024-09-13 19:57:16 +0900

    add quit button

--- nodes/scenes/title_scene.gd
+++ nodes/scenes/title_scene.gd
@@ -13,10 +13,15 @@ func on_menu_map_pressed() -> void:
 	scene_changed.emit("title", "map")
 
 
+func on_menu_quit_pressed() -> void:
+	get_tree().quit()
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Centering/MenuGame.pressed.connect(on_menu_game_pressed)
 	$Centering/MenuMap.pressed.connect(on_menu_map_pressed)
+	$Centering/MenuQuit.pressed.connect(on_menu_quit_pressed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
